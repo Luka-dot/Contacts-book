69dc52883635f187ef611f51f13ece09100a48c1
Luka-dot <79lukask@gmail.com>
2021-03-13 19:18:05 -0500

    DELETE contacts connected to back end

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -39,14 +39,18 @@ const ContactState = props => {
             dispatch({ type: ADD_CONTACT, payload: res.data });
         } catch (err) {
             dispatch({ type: CONTACT_ERROR, payload: err.response.msg })
-        }
-
-        
+        } 
     };
 
     // DELETE contact
-    const deleteContact = (id) => {
-        dispatch({ type: DELETE_CONTACT, payload: id });
+    const deleteContact = async (id) => {
+        try {
+            await axios.delete(`/api/contacts/${id}`);
+            dispatch({ type: DELETE_CONTACT, payload: id });
+        } catch (err) {
+            dispatch({ type: CONTACT_ERROR, payload: err.response.msg })
+        }
+        
     };
 
     // CLEAR contacts
